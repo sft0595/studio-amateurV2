029be3a85b66b97f97e5155eda18add84710044d
sft0595 <shafayet.rabby@gmail.com>
2025-02-11 17:17:15 +0600

    mod 2

--- pages/index.vue
+++ pages/index.vue
@@ -42,7 +42,7 @@
 
     <article
       class="flex items-center justify-center p-5 overflow-hidden"
-      style="max-width: 1200px; margin: 0 auto"
+      style="max-width: 1200px; margin: 150px auto"
       v-gsap.whenVisible.from="{ scale: 0.8, height: 0, padding: 0 }"
     >
       <div
@@ -57,11 +57,12 @@
       </div>
       <div
         v-gsap.whenVisible.delay-1000.stagger.from="{
-          opacity: 0,
-          x: -50,
-          duration: 1,
-          ease: 'power2.out',
-          scale: 0.8,
+          opacity: 1,
+          x: '100%',
+          delay: 0.5,
+          duration: 2,
+          ease: 'power2.inOut',
+          scale: 1,
         }"
         class="flex-auto p-5"
       >
@@ -112,41 +113,222 @@
       </div>
     </article>
 
-    <section class="my-8">
-      <div class="flex justify-around">
-        <div class="p-10">
-          <p class="text-3xl">Space Research</p>
-          <p class="text-xl">
-            We have to research space to make your house, office, restaurant,
-            shopping mall, hotel or any space usable and aesthetic. To do this
-            research we follow a few steps, such as first we visit your space
-            and collect its blueprint with measurement and then we do 2D
-            planning in our studio considering the client’s best interest, and
-            then we transform it into 3D for visualization.
+    <section style="margin: 150px auto">
+      <div class="lg:flex justify-end align-baseline">
+        <div class="px-10">
+          <p class="lg:text-8xl text-5xl font-bold lg:text-right">
+            The Research Process
           </p>
         </div>
-
-        <div class="flex-none gap-5 relative">
-          <p class="text-8xl font-bold">Research</p>
-            <img
-              src="/assets/images/creative-min.jpg"
-              alt="Chatta Metro Restaurant chittagong, chattogram"
-            />
+        <div
+          style="
+            min-width: 350px;
+            min-height: 400px;
+            background-image: url('_nuxt/assets/images/creative-min.jpg');
+            background-size: cover;
+            background-position: bottom, center;
+            background-repeat: no-repeat;
+          "
+          class="m-10 lg:m-0 overflow-hidden"
+          v-gsap.whenVisible.from="{ opacity: 0, y: '40%' }"
+        >
+          <!-- <img
+            class="w-full hidden lg:block"
+            src="/assets/images/creative-min.jpg"
+            alt="Chatta Metro Restaurant chittagong, chattogram"
+          /> -->
+        </div>
+        <div class="" style="max-width: 900px">
+          <div class="px-10 mb-10">
+            <p class="text-3xl font-bold mb-3">Space Research</p>
+            <p class="text-xl text-justify">
+              We have to research space to make your house, office, restaurant,
+              shopping mall, hotel or any space usable and aesthetic. To do this
+              research we follow a few steps, such as first we visit your space
+              and collect its blueprint with measurement and then we do 2D
+              planning in our studio considering the client’s best interest, and
+              then we transform it into 3D for visualization.
+            </p>
           </div>
+          <div class="px-10 mb-10">
+            <p class="text-3xl font-bold mb-3">Material Research</p>
+            <p class="text-xl text-justify">
+              Martial play an important role in the implementation of work. In
+              most cases, it is seen that due to not knowing the proper use of
+              the material according to the place, it gets wasted very quickly
+              or become unusable. In that case, before using the material,
+              Studio Amateur test the material in its studio to determine its
+              durability, usability, aesthetics and various advantage and
+              disadvantage. The Studio Amateur uses its materials after being
+              100% sure about it. No one can 100% guarantee about a material
+              (usually wood falls in this category) acquired from a natural
+              source. In that case, Studio Amateur takes appropriate measures to
+              ensure its quality before use.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
 
-        <div class="p-10">
-          
+    <section style="display: flex; flex-direction: column">
+      <div
+        class="projectHead flex items-center justify-start"
+        style="
+          width: 90%;
+          height: 240px;
+          background-position: center center;
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-image: url('_nuxt/assets/images/bg/apartments.webp');
+          margin-left: auto;
+          margin-right: 0;
+        "
+        v-gsap.whenVisible.from="{
+          opacity: 0,
+          x: '40%',
+          duration: 2,
+        }"
+      >
+        <a href="" class="relative block">
+          <p class="mx-10 text-5xl underline font-bold" style="color: #f5f5dc">
+            Appartments
+          </p>
+        </a>
+      </div>
 
-          <p class="text-3xl">Material Research</p>
-          <p class="text-xl">
-            Martial play an important role in the implementation of work. In
-            most cases, it is seen that due to not knowing the proper use of the
-            material according to the place, it gets wasted very quickly or
-            become unusable. In that case, before using the material, Studio
-            Amateur test the material in its studio to determine its durability,
-            usability, aesthetics and various advantage and disadvantage.
+      <div
+        class="projectHead flex items-center justify-end"
+        style="
+          width: 90%;
+          height: 240px;
+          background-position: center center;
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-image: url('_nuxt/assets/images/bg/Office-Interior.jpg');
+          margin-right: auto;
+        "
+        v-gsap.whenVisible.delay-1000.from="{
+          opacity: 0,
+          x: '-40%',
+          duration: 2,
+        }"
+      >
+        <a href="" class="relative block">
+          <p class="mx-10 text-5xl underline font-bold" style="color: #f5f5dc">
+            Office Interior
+          </p>
+        </a>
+      </div>
+
+      <div
+        class="projectHead flex items-center justify-start"
+        style="
+          width: 90%;
+          height: 240px;
+          background-position: center center;
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-image: url('_nuxt/assets/images/bg/restaurants.webp');
+          margin-left: auto;
+        "
+        v-gsap.whenVisible.from="{
+          opacity: 0,
+          x: '40%',
+          duration: 2,
+        }"
+      >
+        <a href="" class="relative block">
+          <p class="mx-10 text-5xl underline font-bold" style="color: #f5f5dc">
+            Restaurants
+          </p>
+        </a>
+      </div>
+      <div
+        class="projectHead flex items-center justify-end"
+        style="
+          width: 90%;
+          height: 240px;
+          background-position: center center;
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-image: url('_nuxt/assets/images/j to d/jtod (10).jpg');
+          margin-right: auto;
+        "
+        v-gsap.whenVisible.from="{
+          opacity: 0,
+          x: '-40%',
+          duration: 2,
+        }"
+      >
+        <a href="" class="relative block">
+          <p class="mx-10 text-5xl underline font-bold" style="color: #f5f5dc">
+            Paintings
+          </p>
+        </a>
+      </div>
+    </section>
+
+    <section style="margin-top: 150px">
+      <div class="lg:flex justify-end items-center">
+        <!-- Title -->
+        <div class="px-10">
+          <p
+            class="lg:text-8xl text-5xl font-bold lg:text-right"
+            v-gsap.whenVisible.from="{ opacity: 0, x: '-40%' }"
+          >
+            Director's Message
           </p>
         </div>
+
+        <!-- Image Container -->
+        <div
+          style="max-width: 400px"
+          class="m-10 lg:m-0 overflow-hidden"
+          v-gsap.whenVisible.from="{ opacity: 0, y: '40%' }"
+        >
+          <img
+            src="/assets/images/sharfuddin-mahamood-chowdhury.png"
+            alt="Chatta Metro Restaurant chittagong, chattogram"
+          />
+        </div>
+
+        <!-- Description -->
+        <div
+          class="max-w-3xl"
+          v-gsap.whenVisible.from="{ opacity: 0, x: '40%' }"
+        >
+          <div class="px-10 mb-10">
+            <p class="text-xl text-justify">
+              <i>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+                nam ducimus aliquid, fugiat quos, hic nihil perspiciatis laborum
+                sint optio iste assumenda tenetur itaque voluptatum provident
+                dicta sapiente quidem porro? Repellendus quia dignissimos eius
+                porro omnis, incidunt ad dolore reiciendis ipsa aspernatur
+                officia totam ullam delectus quas quos molestiae ut nemo dolores
+                ea facilis rem! Eligendi ratione magni at alias! Numquam
+                eligendi nihil quas suscipit rem animi, dolorum magni eveniet
+                illo mollitia laboriosam quasi sequi provident, ullam quo!
+                Provident quo voluptate at mollitia, consequatur reiciendis
+                delectus quidem dicta vitae itaque. Explicabo natus tempora ad
+                aliquam accusantium quisquam ipsam doloremque in debitis earum,
+                asperiores incidunt numquam laborum hic provident consequuntur
+                fugiat beatae? Facere, vel deserunt! At blanditiis quidem fugit
+                sit nihil! Repellendus vero aliquam ab rerum incidunt
+                exercitationem voluptas quas delectus rem molestias. Aspernatur
+                eaque ex tempora quidem soluta. Eum, deleniti ut.
+              </i>
+              <br /><br />
+              - Sharfuddin Mahamood Chowdhury
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="contact-image">
+        <img src="/assets/images/footer.jpg" alt="Contact us" class="w-full" />
       </div>
     </section>
 
@@ -190,4 +372,25 @@
 /* .fontSquad {
   font-family: "Squada One", serif;
 } */
+
+.projectHead {
+  margin-top: 2px;
+}
+.projectHead:nth-child(odd)::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background: linear-gradient(-90deg, rgba(80, 48, 27, 0.8), transparent);
+}
+
+.projectHead:nth-child(even)::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background: linear-gradient(-90deg, rgba(80, 48, 27, 0.8), transparent);
+}
 </style>
